93b855c3a9ba5a34b9b6e3c6ad5637a2de613a2f
AlexandrKostetki <70638083+AlexandrKostetki@users.noreply.github.com>
2021-10-11 10:03:17 +0300

    Add files via upload

---
+++ 11.10.2021.py
@@ -0,0 +1,4 @@
+nume=str(input('Ce nume si prenume ai?\n '))
+profesia=str(input('Ce iti doresti sa fii in viitor?\n'))
+print('Te numesti \n{}'.format(nume))
+print('Doresti sa devii \n{}\n'.format(profesia))
